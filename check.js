c0e099699379d9122c026979539083019a1d490a
Oliver Cooper <oliver.cooper@me.com>
2019-10-11 19:40:55 +1300

    Added checking multi-show

--- check.js
+++ check.js
@@ -16,27 +16,35 @@ process.on ('exit', code => {
   exec(`sh ${ path.join(__dirname, "stop-proxy.sh") }`)
 });
 
-fetch("https://10play.com.au/have-you-been-paying-attention").then(response => response.text()).then(async page => {
-  const pageData = page.match(/<script>const\s+showPageData\s*=\s*(\{.+\})\;<\/script>/)
-  if (pageData) {
-    const show = JSON.parse(pageData[1])
-    const latestVideo = show.video.videoId
-    if (!existingEpisodes.hybpa[latestVideo]) {
-      console.log("New episode!", show.video.title, latestVideo)
-      const tempPath = await download(latestVideo)
-      const [_, seasonNo, episodeNo] = show.video.title.match(/S.*?(\d+) E.*?(\d+)/)
-      const fileName = `Have You Been Paying Attention! - ${ DateTime.fromISO(show.video.airDate).toISODate() } - Episode ${ episodeNo }.mkv`
-      const destPath = `/media/plex/tv/Have You Been Paying Attention!/Season ${ seasonNo }/${ fileName }`
-      mv(tempPath, destPath, err => {
-        if (err) { throw err }
-        existingEpisodes.hybpa[latestVideo] = true
-        saveEpisodes()
-        console.log("Moved episode to", `/media/plex/tv/Have You Been Paying Attention!/${ fileName }`)
-        exec(`sh ${ path.join(__dirname, "stop-proxy.sh") }`)
-      })
+async function checkShow(id, url, folderName, formatFileName) {
+  console.log("Checking show:", id)
+  existingEpisodes[id] = existingEpisodes[id] || {}
+  await fetch(url).then(response => response.text()).then(async page => {
+    const pageData = page.match(/<script>const\s+showPageData\s*=\s*(\{.+\})\;<\/script>/)
+    if (pageData) {
+      const show = JSON.parse(pageData[1])
+      const latestVideo = show.video.videoId
+      if (!existingEpisodes[id][latestVideo]) {
+        console.log("New episode!", show.video.title, latestVideo)
+        const tempPath = await download(latestVideo)
+        const [_, seasonNo, episodeNo] = show.video.title.match(/S.*?(\d+) E.*?(\d+)/)
+        const fileName = formatFileName(show.video, episodeNo, seasonNo)
+        const destPath = `/media/plex/tv/${ folderName }/Season ${ seasonNo }/${ fileName }`
+        mv(tempPath, destPath, err => {
+          if (err) { throw err }
+          existingEpisodes[id][latestVideo] = true
+          saveEpisodes()
+          console.log("Moved episode to", `/media/plex/tv/${ folderName }/${ fileName }`)
+          exec(`sh ${ path.join(__dirname, "stop-proxy.sh") }`)
+        })
+      }
+      else {
+        console.log("No new episode, latest: ", show.video.title)
+      }
     }
-    else {
-      console.log("No new episode, latest: ", show.video.title)
-    }
-  }
-})
+  })
+  
+}
+
+checkShow("hybpa", "https://10play.com.au/have-you-been-paying-attention", "Have You Been Paying Attention!", (video, episodeNo) => `Have You Been Paying Attention! - ${ DateTime.fromISO(video.airDate).toISODate() } - Episode ${ episodeNo }.mkv`)
+checkShow("bachelorette", "https://10play.com.au/the-bachelorette", "The Bachelorette Australia", (video, episodeNo, seasonNo) => `The Bachelorette Australia - ${ seasonNo }x0${ episodeNo } - Episode ${ episodeNo }.mkv`)
